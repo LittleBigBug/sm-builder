48ddfe856a6d2172d811b25862581a3f2762cc9f
LittleBigBug <ethan@yasfu.net>
2024-11-13 01:37:38 -0700

    fix: change repo in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    url='https://github.com/splewis/sm-builder',
+    url='https://github.com/LittleBigBug/sm-builder',
     scripts=['scripts/smbuilder', 'scripts/smstruct', 'scripts/smversion'],
     package_data={'smbuilder': ['plugins/*.sp', 'plugins/smbuild']},
 )
